603995bafcdf03bec4780374f14e6bd664c7b742
carleii2 <devcarle@gmail.com>
2025-08-26 16:31:21 +0100

    k

--- k/connexion.js
+++ k/connexion.js
@@ -13,6 +13,6 @@ function anderson() {
     }
     if(commercant.password == password){
         alert("connexion reussi✅.");
-     window.location.href = "./k/loader2.html";
+     window.location.href = "loader2.html";
     }
 }
